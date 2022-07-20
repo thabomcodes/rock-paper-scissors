738ba0f9a126f5f5e46c0e37d791feb79f6c75cb
Thabo Moloi <thabomoloicodes@gmail.com>
2022-07-20 05:07:16 +0200

    Modified the Event Listeners

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const rps = ["rock", "paper", "scissors"];
     const choice = Math.floor(Math.random() * 3);
@@ -9,28 +12,35 @@ function playRound(playerSelection, computerSelection) {
     const computerMove = computerSelection.toLowerCase();
 
     if (playerMove == computerMove) {
-        return `It's a tie! ${playerMove} ties with ${computerMove}.`;
+        return 'tie';
     }
     else if ((playerMove == "rock" && computerMove == "paper") ||
         (playerMove == "paper" && computerMove == "scissors") ||
         (playerMove == "scissors" && computerMove == "rock")) {
-        return `You lose! ${playerMove} is beaten by ${computerMove}.`;
+        computerScore++;
+        return 'lose';
     }
     else if ((playerMove == "scissors" && computerMove == "paper") ||
         (playerMove == "rock" && computerMove == "scissors") ||
         (playerMove == "paper" && computerMove == "rock")) {
-        return `You win! ${playerMove} beats ${computerMove}.`;
+        playerScore++;
+        return 'win';
     }
     else {
         return "Error!";
     }
 }
 
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-    // add event listener for each button
-    button.addEventListener('click', () => {
-        alert(button.id);
-    });
+function playerChooses(choice) {
+    roundOutcome = playRound(choice, getComputerChoice());
+}
 
-});
+const rockBtn = document.querySelector("button#rock");
+const paperBtn = document.querySelector("button#paper");
+const scissorsBtn = document.querySelector("button#scissors");
+
+/* Event Listener */
+rockBtn.addEventListener('click', () => playerChooses('rock'))
+paperBtn.addEventListener('click', () => playerChooses('paper'))
+scissorsBtn.addEventListener('click', () => playerChooses('scissors'))
+
